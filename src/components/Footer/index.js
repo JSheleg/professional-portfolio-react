ce36f6b2a7857e300ff912550fa57d983d2c9012
JSheleg <jessica.sheleg@gmail.com>
2021-10-11 21:34:10 -0500

    updated footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,30 +1,19 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import * as React from 'react';
-import { Link} from "react-router-dom";
+import { Navbar, Container,Nav } from 'react-bootstrap'
+
 
 const Footer = () => {
   return (
-    <div >
-      <ul className="footer">
-        <li>
-            <Link href = "https://github.com/JSheleg">
-                GitHub
-            </Link>
-        </li>
-        <li>
-            <Link href ="https://www.linkedin.com/in/jessicasheleg/">
-                LinkedIn
-            </Link>
-        </li>
-        <li>
-            <Link href ="#">
-                TBD
-            </Link>
-        </li>
-      </ul>
-    </div>
-
-      
+    <Navbar bg="dark" variant="dark" className="Footer">
+      <Container>
+        <Nav className="footer">
+          <Nav.Link href="https://github.com/JSheleg">GitHub</Nav.Link>
+          <Nav.Link href="https://www.linkedin.com/in/jessicasheleg/">LinkedIn</Nav.Link>
+          <Nav.Link href="#">TBD</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>      
   )
 }
 
