c0d2b5e2b9588671ab4eeb2d7542ecfdc255934e
JSheleg <jessica.sheleg@gmail.com>
2021-10-14 08:46:39 -0500

    update package.json

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React from "react";
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router,Route, Switch } from 'react-router-dom';
 import About from './components/pages/About';
 import Portfolio from './components/pages/Portfolio';
 import Resume from './components/pages/Resume';
@@ -12,18 +12,18 @@ import './App.css';
 
 function App() {
     return(
-        <HashRouter basename = '/'>
+        <Router >
             <div>
                 <Nav/>
                     <Switch>
                         <Route exact path = '/' component={About}/>
-                        <Route path = '/portfolio' component={Portfolio}/>
-                        <Route path = '/resume' component={Resume}/>
-                        <Route path = '/contact' component={Contact}/>
+                        <Route exact path = '/portfolio/' component={Portfolio}/>
+                        <Route exact path = '/resume/' component={Resume}/>
+                        <Route exact path = '/contact/' component={Contact}/>
                     </Switch>
                 <Footer/>
             </div>
-        </HashRouter>
+        </Router>
     );
 }
 
